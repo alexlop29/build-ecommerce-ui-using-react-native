59962f01e8f065a30806b53da099be6b8d3bb36e
alopez-axiom <alopez@axiomcloud.ai>
2023-10-19 16:11:18 -0400

    Add bottom navigation menu

--- build-ecommerce-ui/App.tsx
+++ build-ecommerce-ui/App.tsx
@@ -1,23 +1,54 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import WishList from "./pages/WishList";
+import Account from "./pages/Account";
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Cart"
+          component={Cart}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="shopping-cart" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="WishList"
+          component={WishList}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="heart" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Account"
+          component={Account}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="user" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
